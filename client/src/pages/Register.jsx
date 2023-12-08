51b6f8de9d6caeef07f446ace1a29992f96cf63b
zntb <zntbdev@proton.me>
2023-12-08 17:03:08 +0200

    Link Component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Register = () => {
-  return <h1>Register Page</h1>;
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <Link to="/login">Login Page</Link>
+    </div>
+  );
 };
 export default Register;
