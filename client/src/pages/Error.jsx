4c7ed013bd6ea1767f956b9a23fbb138a7fa78d0
zntb <zntbdev@proton.me>
2023-12-10 16:25:49 +0200

    Add elements and styles to Error.jsx page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,13 +1,29 @@
 import { Link, useRouteError } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh! page not found.</h3>
+          <p>We can&apos;t seem to find the page you&apos;re looking for</p>
+          <Link to="/dashboard">back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to="/">back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong!</h3>
+      </div>
+    </Wrapper>
   );
 };
+
 export default Error;
