cbf997b23483db483dda6ca91168c95554456d20
zntb <zenetibi@yahoo.com>
2023-12-12 17:04:43 +0200

    Add FormRow Component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
-import { Logo } from '../components';
+import { FormRow, Logo } from '../components';
 
 const Register = () => {
   return (
@@ -8,19 +8,16 @@ const Register = () => {
       <form className="form">
         <Logo />
         <h4>Register</h4>
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="form-input"
-            defaultValue="john"
-            required
-          />
-        </div>
+        <FormRow type="text" name="name" defaultValue="" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue=""
+        />
+        <FormRow type="text" name="location" defaultValue="" />
+        <FormRow type="email" name="email" defaultValue="" />
+        <FormRow type="password" name="password" defaultValue="" />
         <button type="submit" className="btn btn-block">
           submit
         </button>
