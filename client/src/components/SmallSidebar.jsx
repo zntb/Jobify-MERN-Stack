2b86fb0ee27a27eb35fac0932878263f692e022d
zntb <zntbdev@proton.me>
2023-12-13 16:37:45 +0200

    Setting Dashboard Context

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,6 +1,9 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
 const SmallSidebar = () => {
+  const data = useDashboardContext();
+  console.log(data);
   return <Wrapper>Smallsidebar</Wrapper>;
 };
 export default SmallSidebar;
