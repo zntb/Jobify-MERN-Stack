1018ef372ce81c053ee9e2217ce5763bf2394cbd
zntb <zntbdev@proton.me>
2023-12-17 12:38:07 +0200

    Setup Big Sidebar

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,7 +1,27 @@
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import { useDashboardContext } from '../pages/DashboardLayout';
 
 const BigSidebar = () => {
-  return <Wrapper>Bigsidebar</Wrapper>;
+  const { showSidebar } = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks isBigSidebar />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default BigSidebar;
