b0c4acda838eb28d85fa00f2cc7120b3f29db9db
zntb <zenetibi@yahoo.com>
2023-12-17 14:46:47 +0200

    Add Dark Theme Logic

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 import Wrapper from '../assets/wrappers/Dashboard';
 import { BigSidebar, Navbar, SmallSidebar } from '../components';
+import { checkDefaultTheme } from '../App';
 
 const DashBoardContext = createContext();
 
@@ -10,15 +11,17 @@ const DashboardLayout = () => {
   // temp
   const user = { name: 'john' };
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
 
   const toggleDarkTheme = () => {
-    console.log('toggle dark theme');
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
   };
 
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
-    console.log('toggle sidebar');
   };
 
   const logoutUser = async () => {
