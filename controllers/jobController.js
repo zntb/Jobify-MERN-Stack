674ceb26a28330263f33126821f8803ca579d99d
zntb <zenetibi@yahoo.com>
2024-02-05 18:02:45 +0200

    Add async error handler

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -12,7 +12,7 @@ export const getAllJobs = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
-  const job = await Job.create(req.body);
+  const job = await Job.create('req.body');
   res.status(201).json({ job });
 };
 
