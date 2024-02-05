d00790337fbf389a84f3f79deb3fa4a3bc8db872
zntb <zntbdev@proton.me>
2024-02-05 18:16:31 +0200

    Get All Jobs and Get Single Job

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -8,17 +8,19 @@ let jobs = [
 ];
 
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
   res.status(200).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
-  const job = await Job.create('req.body');
+  const job = await Job.create(req.body);
   res.status(201).json({ job });
 };
 
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
+
+  const job = await Job.findById(id);
   if (!job) {
     return res.status(404).json({ msg: `No job with id ${id}` });
   }
