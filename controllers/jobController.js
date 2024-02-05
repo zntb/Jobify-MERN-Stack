3ec5706e7950367e497a492b76ec40537e4a8ca3
zntb <zntbdev@proton.me>
2024-02-05 17:46:48 +0200

    Update jobController

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,3 +1,5 @@
+import Job from '../models/JobModel.js';
+
 import { nanoid } from 'nanoid';
 
 let jobs = [
@@ -10,17 +12,8 @@ export const getAllJobs = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
-
-  if (!company || !position) {
-    return res
-      .status(400)
-      .json({ msg: 'Please provide company and position!' });
-  }
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
-  res.status(201).json({ jobs });
+  const job = await Job.create(req.body);
+  res.status(201).json({ job });
 };
 
 export const getJob = async (req, res) => {
