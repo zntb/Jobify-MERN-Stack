2e88a60a117414a4883879cc4f6132d93540f061
zntb <zenetibi@yahoo.com>
2024-02-05 12:52:53 +0200

    Add Get Single Job Functionality

--- server.js
+++ server.js
@@ -45,7 +45,17 @@ app.post('/api/v1/jobs', (req, res) => {
   const id = nanoid(10);
   const job = { id, company, position };
   jobs.push(job);
-  res.status(200).json({ jobs });
+  res.status(201).json({ jobs });
+});
+
+// Get Single Job
+app.get('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `No job with id ${id}` });
+  }
+  res.status(200).json({ job });
 });
 
 const port = process.env.PORT || 5000;
