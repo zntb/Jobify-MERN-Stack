ae0fca5e466ae782ed10c0a1fbbd5b3d5944250a
zntb <zntbdev@proton.me>
2024-02-07 17:59:49 +0200

    Add Http Only Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,13 @@ export const login = async (req, res) => {
     user && (await comparePassword(req.body.password, user.password));
   if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
 
-  const token = createJWT({ userId: user._id, role: user.role });
+  const oneDay = 1000 * 60 * 60 * 24;
 
-  res.json({ token });
+  const token = createJWT({ userId: user._id, role: user.role });
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  });
+  res.status(StatusCodes.OK).json({ msg: 'user logged in' });
 };
