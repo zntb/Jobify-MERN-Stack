cf80227e6a1549dc5bd0aaee5b85d349ca375556
zntb <zenetibi@yahoo.com>
2024-02-07 16:26:23 +0200

    Hash Password restructuring to utils

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,16 @@
-import bcrypt from 'bcryptjs';
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ msg: 'user created' });
+  res.status(StatusCodes.CREATED).json({ user, msg: 'user created' });
 };
 
 export const login = async (req, res) => {
