1784e4de0530ea79790d1b7b56bd86843932e578
zntb <zntbdev@proton.me>
2024-02-07 11:47:54 +0200

    Vlaidate Id Params

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -8,13 +8,16 @@ import {
   updateJob,
   deleteJob,
 } from '../controllers/jobController.js';
-import { validateJobInput } from '../middleware/validationMiddleware.js';
+import {
+  validateJobInput,
+  validateIdParam,
+} from '../middleware/validationMiddleware.js';
 
 router.route('/').get(getAllJobs).post(validateJobInput, createJob);
 router
   .route('/:id')
-  .get(getJob)
-  .patch(validateJobInput, updateJob)
-  .delete(deleteJob);
+  .get(validateIdParam, getJob)
+  .patch(validateJobInput, validateIdParam, updateJob)
+  .delete(validateIdParam, deleteJob);
 
 export default router;
