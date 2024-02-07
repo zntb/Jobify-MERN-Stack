7f0c829ea49dd8ad0aa6de0963d62df15e1cd4ad
zntb <zenetibi@yahoo.com>
2024-02-07 16:41:12 +0200

    Validate Login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import { hashPassword } from '../utils/passwordUtils.js';
+import { UnauthenticatedError } from '../errors/customErrors.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -14,5 +15,5 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  await res.send('login');
+  const user = await User.findOne({ email: req.body.email });
 };
