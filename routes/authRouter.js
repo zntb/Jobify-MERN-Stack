1b31253f05e5dfffb4225d66add4ee5c9b0040bc
zntb <zntbdev@proton.me>
2024-02-07 16:41:12 +0200

    Validate Login

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,9 +2,12 @@ import { Router } from 'express';
 const router = Router();
 
 import { register, login } from '../controllers/authController.js';
-import { validateRegisterInput } from '../middleware/validationMiddleware.js';
+import {
+  validateRegisterInput,
+  validateLoginInput,
+} from '../middleware/validationMiddleware.js';
 
 router.post('/register', validateRegisterInput, register);
-router.post('/login', login);
+router.post('/login', validateLoginInput, login);
 
 export default router;
