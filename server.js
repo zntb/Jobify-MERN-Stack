e390f7b91698f88dc0d7109de926f03fda09e6b7
zntb <zntbdev@proton.me>
2024-02-08 16:54:58 +0200

    Add User Routes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ import cookieParser from 'cookie-parser';
 // Routers
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
+import userRouter from './routes/userRouter.js';
 
 // Middlewares
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
@@ -22,7 +23,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -30,6 +31,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
+app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.use('*', (req, res) => {
