4aa083f9508add183de56b88c46c8d8d01547e6c
zntb <zenetibi@yahoo.com>
2024-02-08 17:32:01 +0200

    Get Application Stats

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,7 @@
-import { UnauthenticatedError } from '../errors/customErrors.js';
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+} from '../errors/customErrors.js';
 import { verifyJWT } from '../utils/tokenUtils.js';
 
 export const authenticateUser = (req, res, next) => {
@@ -13,4 +16,13 @@ export const authenticateUser = (req, res, next) => {
   } catch (error) {
     throw new UnauthenticatedError('authentication invalid');
   }
+};
+
+export const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthorizedError('Unauthorized to access this route');
+    }
+    next();
+  };
 };
