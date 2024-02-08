e8fb22006cec2033655aebb6d2a212eb7a97d5cd
zntb <zntbdev@proton.me>
2024-02-08 17:20:57 +0200

    Update User

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,5 +13,8 @@ export const getApplicationStats = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
+  const obj = { ...req.body };
+  delete obj.password;
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
   res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,8 +6,9 @@ import {
   getApplicationStats,
   updateUser,
 } from '../controllers/userController.js';
+import { validateUpdateUserInput } from '../middleware/validationMiddleware.js';
 
 router.get('/current-user', getCurrentUser);
 router.get('/admin/app-stats', getApplicationStats);
-router.patch('/update-user', updateUser);
+router.patch('/update-user', validateUpdateUserInput, updateUser);
 export default router;
