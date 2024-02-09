e04e25c757eb62a4709ce32bd2edeca996c7fb55
zntb <zenetibi@yahoo.com>
2024-02-09 14:34:02 +0200

    Add Navigation State

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,6 +4,10 @@ import { FormRow, Logo } from '../components';
 import customFetch from '../utils/customFetch';
 
 const Register = () => {
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -19,8 +23,8 @@ const Register = () => {
         <FormRow type="text" name="location" defaultValue="" />
         <FormRow type="email" name="email" defaultValue="" />
         <FormRow type="password" name="password" defaultValue="" />
-        <button type="submit" className="btn btn-block">
-          submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? 'submitting...' : 'submit'}
         </button>
         <p>
           Already a member?
