639d66c9f5360c0e4d7278453d270fc171480a01
zntb <zntbdev@proton.me>
2024-02-10 15:37:20 +0200

    Add Select Input Values

---
+++ client/src/components/FormRowSelect.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+
+const FormRowSelect = ({ name, labelText, list, defaultValue = '' }) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+      <select
+        name={name}
+        id={name}
+        className="form-select"
+        defaultValue={defaultValue}
+      >
+        {list.map((itemValue) => {
+          return (
+            <option key={itemValue} value={itemValue}>
+              {itemValue}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default FormRowSelect;
