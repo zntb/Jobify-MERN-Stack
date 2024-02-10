f986b95952af7a59cc7f37823ba8e8eed4e10d4d
zntb <zenetibi@yahoo.com>
2024-02-10 15:37:20 +0200

    Add Select Input Values

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,10 +1,11 @@
-import { FormRow } from '../components';
+import { FormRow, FormRowSelect } from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { useOutletContext } from 'react-router-dom';
-import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+
 import { Form, useNavigation, redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import customFetch from '../utils/customFetch';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
 
 const AddJob = () => {
   // const { user } = useOutletContext();
@@ -30,6 +31,18 @@ const AddJob = () => {
             labelText="job location"
             name="jobLocation"
             defaultValue={user.location}
+          />
+          <FormRowSelect
+            labelText="job status"
+            name="jobStatus"
+            defaultValue={JOB_STATUS.PENDING}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            labelText="job type"
+            name="jobType"
+            defaultValue={JOB_TYPE.FULL_TIME}
+            list={Object.values(JOB_TYPE)}
           />
           <button
             type="submit"
