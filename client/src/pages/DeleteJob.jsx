9bb8a6dfaa24a69d5acf2590aedb567da4d66804
zntb <zenetibi@yahoo.com>
2024-02-11 13:14:14 +0200

    Add Delete Job Functionality

--- client/src/pages/DeleteJob.jsx
+++ client/src/pages/DeleteJob.jsx
@@ -1,4 +1,19 @@
+import { redirect } from 'react-router-dom';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
+
 const DeleteJob = () => {
   return <h1>DeleteJob Page</h1>;
 };
 export default DeleteJob;
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const action = async ({ params }) => {
+  try {
+    await customFetch.delete(`/jobs/${params.id}`);
+    toast.success('Job deleted successfully');
+  } catch (error) {
+    toast.error(error.response.data.msg);
+  }
+  return redirect('/dashboard/all-jobs');
+};
