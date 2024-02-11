7f598548a01df53874634abd55b23c9919b0e98f
zntb <zenetibi@yahoo.com>
2024-02-11 16:09:33 +0200

    Setup Multer

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-const router = Router();
 
 import {
   getCurrentUser,
@@ -8,11 +7,19 @@ import {
 } from '../controllers/userController.js';
 import { validateUpdateUserInput } from '../middleware/validationMiddleware.js';
 import { authorizePermissions } from '../middleware/authMiddleware.js';
+import upload from '../middleware/multerMiddleware.js';
+
+const router = Router();
 
 router.get('/current-user', getCurrentUser);
 router.get('/admin/app-stats', [
   authorizePermissions('admin'),
   getApplicationStats,
 ]);
-router.patch('/update-user', validateUpdateUserInput, updateUser);
+router.patch(
+  '/update-user',
+  upload.single('avatar'),
+  validateUpdateUserInput,
+  updateUser
+);
 export default router;
