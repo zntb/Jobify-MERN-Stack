c72dfb926e03341695e00b0e3f0b77bb52b7ebce
zntb <zenetibi@yahoo.com>
2024-02-12 20:57:47 +0200

    Setup Debounce

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -9,6 +9,18 @@ const SearchContainer = () => {
   const { search, jobStatus, jobType, sort } = searchValues;
 
   const submit = useSubmit();
+
+  const debounce = (onChange) => {
+    let timeout;
+    return (e) => {
+      const form = e.currentTarget.form;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        onChange(form);
+      }, 2000);
+    };
+  };
+
   return (
     <Wrapper>
       <Form className="form">
@@ -20,9 +32,9 @@ const SearchContainer = () => {
             type="search"
             name="search"
             defaultValue={search}
-            onChange={(e) => {
-              submit(e.currentTarget.form);
-            }}
+            onChange={debounce((form) => {
+              submit(form);
+            })}
           />
           <FormRowSelect
             labelText="job status"
