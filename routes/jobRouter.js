00835ed4344272a3b3580759d6fd36eb24e6e598
zntb <zenetibi@yahoo.com>
2024-02-12 15:56:46 +0200

    Stats Route Setup

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -7,6 +7,7 @@ import {
   createJob,
   updateJob,
   deleteJob,
+  showStats,
 } from '../controllers/jobController.js';
 import {
   validateJobInput,
@@ -18,6 +19,9 @@ router
   .route('/')
   .get(getAllJobs)
   .post(checkForTestUser, validateJobInput, createJob);
+
+router.route('/stats').get(showStats);
+
 router
   .route('/:id')
   .get(validateIdParam, getJob)
