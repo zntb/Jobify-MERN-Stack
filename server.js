38035575cdfc2f98e1819aa9be37bb3520177fa9
zntb <zenetibi@yahoo.com>
2024-02-13 17:40:41 +0200

    Add Security Packages - helmet, mongoSanitize

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import cloudinary from 'cloudinary';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 
 // Routers
 import jobRouter from './routes/jobRouter.js';
@@ -41,13 +43,8 @@ app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
-
-app.get('/api/v1/test', (req, res) => {
-  res.json({ msg: 'test route' });
-});
+app.use(helmet());
+app.use(mongoSanitize());
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
