9c9d7b47f4f3ab053d684ea9bef1b658c0733d4f
zntb <zntbdev@proton.me>
2024-02-13 14:59:00 +0200

    Fix Update User

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -10,16 +10,11 @@ const Register = () => {
       <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
-        <FormRow type="text" name="name" defaultValue="" />
-        <FormRow
-          type="text"
-          name="lastName"
-          labelText="last name"
-          defaultValue=""
-        />
-        <FormRow type="text" name="location" defaultValue="" />
-        <FormRow type="email" name="email" defaultValue="" />
-        <FormRow type="password" name="password" defaultValue="" />
+        <FormRow type="text" name="name" />
+        <FormRow type="text" name="lastName" labelText="last name" />
+        <FormRow type="text" name="location" />
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
         <SubmitBtn />
         <p>
           Already a member?
