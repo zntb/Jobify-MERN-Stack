8931ab34c9f29721bb08bfc6f592db14ed293f44
zntb <zenetibi@yahoo.com>
2024-02-13 14:59:00 +0200

    Fix Update User

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
 app.use(express.json());
 
@@ -52,6 +52,10 @@ app.get('/api/v1/test', (req, res) => {
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+});
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
